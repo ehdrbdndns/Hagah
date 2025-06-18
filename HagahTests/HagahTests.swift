8149bbfb6ff6ca1d52ee508b54852ad40ba1868f
ehdrbdndns <ehdrbdndns@naver.com>
2025-06-18 15:56:17 +0900

    Initial Commit

---
+++ HagahTests/HagahTests.swift
@@ -0,0 +1,17 @@
+//
+//  HagahTests.swift
+//  HagahTests
+//
+//  Created by Donggyun Yang on 6/18/25.
+//
+
+import Testing
+@testable import Hagah
+
+struct HagahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
